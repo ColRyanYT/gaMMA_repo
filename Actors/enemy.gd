257c64109a91536034f85a500645f7c7ea93f972
ColRyanYT <colum.ryan15@gmail.com>
2025-11-04 16:44:34 +1100

    added some basic enemy ai that follows the player

---
+++ Actors/enemy.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed = 200
+@export var player: Node2D
+
+@onready var navigation_agent = $NavigationAgent2D
+
+func _physics_process(_delta: float) -> void:
+	var next_path_pos = navigation_agent.get_next_path_position()
+	var direction = global_position.direction_to(next_path_pos)
+	velocity = direction * speed
+	move_and_slide()
+
+func pathfind() -> void:
+	navigation_agent.target_position = player.global_position
+
+func _on_timer_timeout() -> void:
+	if player.global_position.distance_to(self.global_position) < 400:
+		pathfind()
